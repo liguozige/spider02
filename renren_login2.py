470e5f143813eaeb004f0662b3dd632befd6c81c
lgl <lgl@163.com>
2018-07-10 10:13:27 +0800

    請求頭中帶cookie登錄人人網更正

---
+++ renren_login2.py
@@ -0,0 +1,21 @@
+import requests
+
+
+def renren_profile():
+    #1.目标url 人人好友页面
+    profile_url = 'http://www.renren.com/965207546/profile'
+
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko",
+        "Cookie":""
+    }
+    #发送请求
+    data = requests.get(profile_url,headers=headers).content.decode()
+
+    #写入本地
+    with open('renren.html','w')as f:
+        f.write(data)
+
+
+if __name__ == '__main__':
+    renren_profile()
